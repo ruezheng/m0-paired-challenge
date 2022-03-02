04bfb7c1fb91d8ceacc89faf6001f5b6715aad8c
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-03-01 20:39:58 -0700

    Initial commit

---
+++ challenge/challenge.rb
@@ -0,0 +1,13 @@
+# File is named "challenge.rb because we are doing a challenge in Ruby."
+# variable books = 5
+books = 5
+# array of book_titles
+book_titles = ["book1", "book2", "book3", "book4", "book5"]
+# boolean librarian is present, true
+librarian = true
+# variable book_price is 10.99
+book_price = 10.99
+# variable number_of_pages is 250
+number_of_pages = 250
+# variable librarian_name is Nancy
+librarian_name = "Nancy"
